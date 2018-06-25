142e167fe8ade0f0daba18df221df188a596d7f7
fabian727 <fabian727@gmx.net>
2018-06-25 22:24:49 +0200

    Update EasyRGB.ino

--- EasyRGB.ino
+++ EasyRGB.ino
@@ -1,188 +1,147 @@
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
+
+#define WIFI
+#define mDNS
+#define WebServer
+//#define WS2812
+#define PWM_LED
+#define AMBI
+#define CONFIG
+
+#include "config.h"
+
+#ifdef WIFI
+  #include <ESP8266WiFi.h>
+    //WiFi
+  #ifndef WLAN_SSID
+    #define WLAN_SSID "YOUR_SSID"
+  #endif
+  #ifndef WLAN_PWD
+    #define WLAN_PWD "YOUR_PWD"
+  #endif
+#endif
+#ifdef PWM_LED
+  #ifndef PIN_R
+    #define PIN_R 12
+  #endif
+  #ifndef PIN_G
+    #define PIN_G 13
+  #endif
+  #ifndef PIN_B
+    #define PIN_B 14
+  #endif
+#endif
+
+#ifdef mDNS
+//mDNS
 #include <ESP8266mDNS.h>
-#include <ESP8266HTTPUpdateServer.h>
-#include <FS.h>
+String  host = "easyrgb";
+#endif
 
-const char* host = "EasyRGB";
-const char* update_path = "/update";
-const char* update_username = "admin";
-const char* update_password = "admin";
+#ifdef WebServer
+  #include "webpage.h"
+  #include <ESP8266WebServer.h>
+  //WebServer
+  ESP8266WebServer httpServer(80);
+  
+  //UpdateServer
+  #include <ESP8266HTTPUpdateServer.h>  
+  ESP8266HTTPUpdateServer httpUpdater;
+  static const char* update_path = "/update";
+  static const char* update_username = "admin";
+  static const char* update_password = "admin";
+#endif
 
-ESP8266WebServer httpServer(80);
-ESP8266HTTPUpdateServer httpUpdater;
+#ifdef CONFIG
+  #include <ArduinoJson.h>
+  #include "FS.h"
 
-//RGB LEDs
-#define PIN_R 2
-#define PIN_G 3
-#define PIN_B 0
-//Tx = GPIO 1
-//Rx = GPIO 3
+  int width =30, height=10,pin1=0,pin2=5;
+bool loadConfig() {
+  File configFile = SPIFFS.open("/config.json", "r");
+  if (!configFile) {
+    Serial.println("Failed to open config file");
+    return false;
+  }
 
-const String PageOne = "<!DOCTYPE html>\
-<html>\
-<meta content=\"text/html; charset=ISO-8859-1\" http-equiv=\"content-type\">\
-<meta charset=\"UTF-8\">\
-<head>\
-  <title>Web RGB</title>\
-<style>\
-body {\
-  background-color: black;\
-  margin: 0px;\
-  padding: 0px;\
-  font-size: 30px;\
-}\
-form {\
-  display: none;\
-}\
-canvas {\
-  width: 100%;\
-  max-width: 1024px;\
-  margin-left: auto;\
-  margin-right: auto;\
-  display: block;\
-}\
-#inner {\
-  position: absolute;\
-  left: 50vw;\
-  top: 36vw;\
-  height: 26vw;\
-  width: 26vw;\
-  transform:translateX(-50%);\
-  border-radius: 100%;\
-  background-color: #838485;\
-  border: 5px solid black;\
-  cursor: pointer;\
-  box-shadow: inset 0 0 15px #";
+  size_t size = configFile.size();
+  if (size > 1024) {
+    Serial.println("Config file size is too large");
+    return false;
+  }
 
-const String PageTwo=";\
-}\
-#inner > svg {\
-  margin-left: 25%;\
-  width:50%;\
-  height:100%;\
-}\
-@media screen and (min-width: 1025px) {\
-  canvas {\
-    max-width: 500px;\
-  }\
-  #inner {\
-    top: calc(250px - 65px); /*funktioniert so gut wie, ab diesem Zeitpunkt*/\
-    height: 130px;\
-    width: 130px;\
-  }\
-}\
-</style>\
-<script>\
-function power() {\
-  document.getElementById(\"power\").checked = !document.getElementById(\"power\").checked;\
-  document.getElementById(\"Form\").submit();\
-}\
-function canvas_create() {\
-  var canvas = document.getElementById(\"picker\");\
-  var width = canvas.offsetWidth;\
-  canvas.width = width;\
-  canvas.height = width;\
-  var context = canvas.getContext(\"2d\");\
-  var x = width/2;\
-  var radius = width/3;\
-  var counterClockwise = false;\
-  for(var angle=0; angle<=360; angle+=1){\
-    var startAngle = (angle-2)*Math.PI/180;\
-    var endAngle = angle * Math.PI/180;\
-    context.beginPath();\
-    context.moveTo(x, x);\
-    context.arc(x, x, radius, startAngle, endAngle, counterClockwise);\
-    context.closePath();\
-    context.fillStyle = 'hsl('+angle+', 100%, 50%)';\
-    context.fill();\
-  }\
-\
-  var gradient = context.createRadialGradient(x, x, x/3.5, x, x, x/2);\
-  gradient.addColorStop(0, 'white');\
-  gradient.addColorStop(1, 'rgba(255,255,255,0)');\
-  context.fillStyle = gradient;\
-  context.fillRect(0, 0, width, width);\
-  gradient = context.createRadialGradient(x, x, x/2.1, x, x, x/1.4);\
-  gradient.addColorStop(0, 'rgba(0,0,0,0)');\
-  gradient.addColorStop(1, 'black');\
-  context.fillStyle = gradient;\
-  context.fillRect(0, 0, width, width);\
-\
-  document.getElementById(\"picker\").addEventListener(\"click\",function(event){\
-    var eventLocation = getEventLocation(this,event);\
-    var context = this.getContext('2d');\
-    var pixelData = context.getImageData(eventLocation.x, eventLocation.y, 1, 1).data; \
-    var hex = (\"000000\" + rgbToHex(pixelData[0], pixelData[1], pixelData[2])).slice(-6);\
-    document.getElementById(\"rgb\").value = hex;\
-    document.getElementById(\"Form\").submit();\
-  },false);\
-}\
-\
-function getElementPosition(obj) {\
-  var curleft = 0, curtop = 0;\
-  if (obj.offsetParent) {\
-    do {\
-      curleft += obj.offsetLeft;\
-      curtop += obj.offsetTop;\
-    } while (obj = obj.offsetParent);\
-    return { x: curleft, y: curtop };\
-  }\
-  return undefined;\
-}\
-function getEventLocation(element,event){\
-    var pos = getElementPosition(element);\
-    return {\
-        x: (event.pageX - pos.x),\
-        y: (event.pageY - pos.y)\
-    };\
-}\
-function rgbToHex(r, g, b) {\
-  if (r > 255 || g > 255 || b > 255)\
-    throw \"Invalid color component\";\
-  return ((r << 16) | (g << 8) | b).toString(16);\
-}\
-</script>\
-</head>\
-<body>\
-  <form id=\"Form\" action=\"\" method=\"GET\">\
-    <input id=\"rgb\" type=\"text\" name=\"rgb\" value=\"";
-const String PageThree="\"></input>\
-    <input id=\"power\" type=\"checkbox\" name=\"on\"";
-    
-const String PageFour="></input>\
-  </form>\
-  <canvas id=\"picker\"></canvas>\
-  <div id=\"inner\" onclick=\"power()\">\
-    <svg viewBox=\"0 0 200 185\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\">\
-      <path style=\"stroke:#000;stroke-width:22;stroke-linecap:round;\" d=\"m 100,20 0,75\"/>\
-      <path style=\"fill:none;stroke:#000;stroke-width:22;stroke-linecap:round\" d=\"M 65 30 A 75 75 0 1 0 135 30\"/>\
-    </svg>\
-  </div>\
-</body>\
-<footer>\
-  <script>\
-    canvas_create();\
-  </script>\
-</footer>\
-</html>";
+  // Allocate a buffer to store contents of the file.
+  std::unique_ptr<char[]> buf(new char[size]);
 
-uint8_t red=0,green=0,blue=0;
+  // We don't use String here because ArduinoJson library requires the input
+  // buffer to be mutable. If you don't use ArduinoJson, you may as well
+  // use configFile.readString instead.
+  configFile.readBytes(buf.get(), size);
 
-void Ereignis_Index()
-{
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& json = jsonBuffer.parseObject(buf.get());
+
+  if (!json.success()) {
+    Serial.println("Failed to parse config file");
+    return false;
+  }
+
+
+  width = atoi(json["width"]);
+  height = atoi(json["height"]);
+  pin1 = atoi(json["Pin1"]);
+  pin2 = atoi(json["Pin2"]);
+  host = (const char *) json["ServerName"];
+
+  saveConfig();
+
+  return true;
+}
+
+bool saveConfig() {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  json["width"] = width;
+  json["height"] = height;
+  json["Pin1"] = pin1;
+  json["Pin2"] = pin2;
+  json["ServerName"] = host;
+
+  File configFile = SPIFFS.open("/config.json", "w");
+  if (!configFile) {
+    Serial.println("Failed to open config file for writing");
+    return false;
+  }
+
+  json.printTo(configFile);
+  return true;
+}
+#endif
+
+#ifdef WS2812
+  #include <Adafruit_NeoPixel.h>
+  #ifndef NUMPIXELS
+    #define NUMPIXELS      4
+  #endif
+  Adafruit_NeoPixel pixels1 = Adafruit_NeoPixel(NUMPIXELS, pin1, NEO_GRBW + NEO_KHZ800);
+  Adafruit_NeoPixel pixels2 = Adafruit_NeoPixel(NUMPIXELS, pin2, NEO_GRBW + NEO_KHZ800);
+#endif
+
+#ifdef WebServer
+void Ereignis_Index() {
   uint8_t r=0,g=0,b=0;
-  String Page=PageOne;
-  String rgb="000000";
+  String Page = PageOne;
+  String rgb = String("000000");
+  uint8_t red=0,green=0,blue=0,white=0;
   if(httpServer.hasArg("rgb") && httpServer.arg("rgb").length() == 6) {
     rgb = httpServer.arg("rgb");
-    int number = (int) strtol( &rgb[0], NULL, 16);
-    // Split them up into r, g, b values
-    red = number >> 16;
-    green = number >> 8 & 0xFF;
-    blue = number & 0xFF;
+    for(int i=0;i<6;i++) {
+      rgb[i] -= '0';
+    }
+    red   = rgb[0]<<8 | rgb[1];
+    green = rgb[2]<<8 | rgb[3];
+    blue  = rgb[4]<<8 | rgb[5];
   }
+
   if(httpServer.hasArg("on") && httpServer.arg("on") == "on") {
     r=red;
     g=green;
@@ -192,85 +151,207 @@ void Ereignis_Index()
     Page+=PageTwo;
     Page+=rgb;
     Page+=PageThree+"checked";
+    #ifdef WS2812
+      for(uint8_t i = 0; i < NUMPIXELS;i++) {
+        pixels1.setPixelColor(i,r,g,b,0);
+      }
+    #endif
+    #ifdef PWM_LED
+      analogWrite(PIN_R,r);
+      analogWrite(PIN_G,g);
+      analogWrite(PIN_B,b);
+    #endif
   } else {
-    Page+="000000";         //inner shadow of power button
-    Page+=PageTwo+"";      //form "rgb" has no value
-    Page+=PageThree+"";   //form "on" is not checked
+    Page+="000000"+PageTwo+PageThree;
   }
-  analogWrite(PIN_R,r);
-  analogWrite(PIN_G,g);
-  analogWrite(PIN_B,b);
   Page += PageFour;
   httpServer.send(200, "text/html", Page);
-}
 
-String FileRead(const char *path) {
-  String content;
-  File file;
-  if (!SPIFFS.open(path, "r")) {
-    return "";
-  }
-  content = file.readStringUntil(EOF);
-  file.close();
-  return content;
-}
-
-bool FileWrite(const char *path, String* content) {
-  File file;
-  if (!SPIFFS.open(path, "w")) {
-    return false;
-  }
-  file.print(*content);
-  file.close();
-}
-  //-------//
- // SETUP //
-//-------//
-void setup(void) {
-  red=0;
-  green=0;
-  blue=0;
-  //Setup File System
-  SPIFFS.begin();
-  //Setup WiFi (if needed with WPS)
-  WiFi.mode(WIFI_STA);
-  if (!WiFi.SSID()) { //on empty SSID (saved on SPI flash)
-    if (WiFi.beginWPSConfig()) { //start WPS
-      if (WiFi.SSID()) {  //re-assure with SSID
-        WiFi.begin(WiFi.SSID().c_str(), WiFi.psk().c_str()); //start WiFi-session
+#ifdef AMBI
+  //if it is used as ambi light
+  if(httpServer.hasArg("r")) {
+    String clr = httpServer.arg("r");
+    if(clr.length()%8==0) {
+      for(int j=0,k=0;j<clr.length();k++) {
+        red   = clr[j++]<<8 | clr[j++];
+        green = clr[j++]<<8 | clr[j++];
+        blue  = clr[j++]<<8 | clr[j++];
+        white = clr[j++]<<8 | clr[j++];
+        #ifdef WS2812
+          pixels1.setPixelColor(k,red,green,blue,white);
+        #endif
       }
     }
-  } else {  //on already known SSID & PWD
-    WiFi.begin(WiFi.SSID().c_str(), WiFi.psk().c_str()); //start WiFi-session
   }
-
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    WiFi.disconnect();  //delete SSID
-    setup();
+  if(httpServer.hasArg("t")) {
+    String clr = httpServer.arg("t");
+    if(clr.length()%8==0) {
+      for(int j=0,k=0;j<clr.length();k++) {
+        red   = clr[j++]<<8 | clr[j++];
+        green = clr[j++]<<8 | clr[j++];
+        blue  = clr[j++]<<8 | clr[j++];
+        white = clr[j++]<<8 | clr[j++];
+        #ifdef WS2812
+          pixels1.setPixelColor(k+height,red,green,blue,white);
+        #endif
+      }
+    }
   }
+  if(httpServer.hasArg("b")) {
+    String clr = httpServer.arg("b");
+    if(clr.length()%8==0) {
+      for(int j=0,k=0;j<clr.length();k++) {
+        red   = clr[j++]<<8 | clr[j++];
+        green = clr[j++]<<8 | clr[j++];
+        blue  = clr[j++]<<8 | clr[j++];
+        white = clr[j++]<<8 | clr[j++];
+        #ifdef WS2812
+          pixels1.setPixelColor(k,red,green,blue,white);
+        #endif
+      }
+    }
+  }
+  if(httpServer.hasArg("l")) {
+    String clr = httpServer.arg("l");
+    if(clr.length()%8==0) {
+      for(int j=0,k=0;j<clr.length();k++) {
+        red   = clr[j++]<<8 | clr[j++];
+        green = clr[j++]<<8 | clr[j++];
+        blue  = clr[j++]<<8 | clr[j++];
+        white = clr[j++]<<8 | clr[j++];
+        #ifdef WS2812
+          pixels1.setPixelColor(k+width,red,green,blue,white);
+        #endif
+      }
+    }
+  }
+  #endif
+  #ifdef WS2812
+    pixels1.show();
+    pixels2.show();
+  #endif
+}
 
-  MDNS.begin(host);
+#ifdef CONFIG
+void Ereignis_Config() {
+  String PageConfig = PageConfig1;
+  PageConfig += height;
+  PageConfig +=PageConfig2;
+  PageConfig += width;
+  PageConfig +=PageConfig3;
+  PageConfig += pin1;
+  PageConfig +=PageConfig4;
+  PageConfig += pin2;
+  PageConfig +=PageConfig5;
+  PageConfig += host;
+  PageConfig +=PageConfig6;
+  httpServer.send(200, "text/html", PageConfig);
+  if(httpServer.hasArg("width")) {
+    width = atoi(httpServer.arg("width").c_str());
+  }
+  if(httpServer.hasArg("height")) {
+    height = atoi(httpServer.arg("height").c_str());
+  }
+#ifdef WS2812
+  if(httpServer.hasArg("height") || httpServer.hasArg("width")) {
+    pixels1.updateLength(height+width);
+    pixels2.updateLength(height+width);
+  }
+  if(httpServer.hasArg("pin1")) {
+    pin1 = atoi(httpServer.arg("pin1").c_str());
+    pixels1.setPin(pin1);
+  }
+  if(httpServer.hasArg("pin2")) {
+    pin2 = atoi(httpServer.arg("pin2").c_str());
+    pixels2.setPin(pin2);
+  }
+#endif
+  if(httpServer.hasArg("ServerName")) {
+    host = httpServer.arg("ServerName");
+  }
+  saveConfig();
+}
+#endif
+#endif
 
+void setup() {
+  Serial.begin(9600);
+
+  #ifdef PWM_LED
+    pinMode(PIN_R, OUTPUT);
+    pinMode(PIN_G, OUTPUT);
+    pinMode(PIN_B, OUTPUT);
+  
+    digitalWrite(PIN_R,LOW);
+    digitalWrite(PIN_G,LOW);
+    digitalWrite(PIN_B,LOW);
+    
+    analogWriteRange(255);
+    analogWrite(PIN_R,100);
+  #endif
+
+//WiFi
+  Serial.println();
+  Serial.println("Up and Running");
+  #ifdef WIFI
+    Serial.print("Connecting to \"");
+    Serial.print(WLAN_SSID);
+    Serial.println("\"");
+    WiFi.setOutputPower(0);
+    WiFi.hostname(host);
+    WiFi.begin(WLAN_SSID, WLAN_PWD);
+  
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      analogWrite(PIN_G,100);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    // Print the IP address
+    Serial.print("My IP: ");
+    Serial.println(WiFi.localIP());
+#endif
+//UpdateServer
+#ifdef WebServer
   httpUpdater.setup(&httpServer, update_path, update_username, update_password);
+  
+//WebServer
   httpServer.on("/", Ereignis_Index);
-
+  #ifdef CONFIG
+  httpServer.on("/config", Ereignis_Config);
+  #endif
   httpServer.begin();
+#endif
 
-  MDNS.addService("http", "tcp", 80);
+//LED strip init black
+  #ifdef WS2812
+    pinMode(pin1,OUTPUT);
+    pixels1.begin();
+    pixels1.setPixelColor(0,0,100,0,0);
+    pixels1.show();
 
-  //LEDs                                                        //not needed for real, but for sure (security)
-  pinMode(PIN_R, OUTPUT);
-  pinMode(PIN_G, OUTPUT);
-  pinMode(PIN_B, OUTPUT);
+    pinMode(pin2,OUTPUT);
+    pixels2.begin();
+    pixels2.show();
+  #endif
 
-  digitalWrite(PIN_R,LOW);
-  digitalWrite(PIN_G,LOW);
-  digitalWrite(PIN_B,LOW);
-
-  analogWriteRange(255);
-
+//MDNS
+#ifdef mDNS
+   MDNS.begin((const char*) &host);
+   #ifdef WebServer
+     MDNS.addService("http","tcp",80);
+   #endif
+#endif
+   analogWrite(PIN_R,0);
+}
+ 
+void loop() {
+  #ifdef WebServer
+    httpServer.handleClient();
+  #endif
+  delay(100);
 }
 
-void loop(void) {
-  httpServer.handleClient();
-}
+
